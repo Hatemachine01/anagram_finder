0db8413a7934d2859b1e0a50c07661a28b39d7f5
Julio <juliodevasquez@gmail.com>
2017-09-13 15:16:14 -0500

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ require 'csv'
+
+def mostrar_diccionary
+   CSV.foreach("words.csv").each do |palabra|
+    words = palabra[0]    
+    cano=words.split("").sort.join
+    Word.create(word: palabra[0], canonical: cano)
+  end
+end
+  mostrar_diccionary
+
